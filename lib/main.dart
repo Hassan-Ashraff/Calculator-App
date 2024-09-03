0f356e3088de095c11896e777e491eda83af82b5
Hassan Ashraf <hassanashraf9339@gmail.com>
2024-09-03 04:55:30 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
-
-import 'package:calculator/Screens/calculator_screen.dart';
+import 'package:calculator_app/Screens/calculator.dart';
 import 'package:flutter/material.dart';
 
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          useMaterial3: true,
+        useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
-        home: Calculator_screen()
+        home: const Calculator()
 
     );
   }
